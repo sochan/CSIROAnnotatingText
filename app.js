6a4d6d742883d34bac4b405f2f8d864873d87a99
tmahalin <tmahalin@deakin.edu.au>
2019-01-09 08:36:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,115 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+//var express = require("express");
+//var cors = require("cors");
+//var bodyParser = require("body-parser");
+//var app = express();
+
+
+var printTerms=[{}];
+var autoSearch=[{}];
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://admin:admin123456@cluster0-shard-00-00-cmwhk.mongodb.net:27017,cluster0-shard-00-01-cmwhk.mongodb.net:27017,cluster0-shard-00-02-cmwhk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("dictionary").findOne({}, function(err, res) {
+    if (err) throw err;
+    console.log("Result is "+ res.term);
+   client.close();
+  });
+ // perform actions on the collection object
+  //client.close();
+  //console.log('Connected to MongoDB');
+});
+/*
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://admin:admin123456@cluster0-shard-00-00-cmwhk.mongodb.net:27017,cluster0-shard-00-01-cmwhk.mongodb.net:27017,cluster0-shard-00-02-cmwhk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("annotator");
+ // var myobj = { name: "Thinesh", password:"thinesh1234",address: "Highway 37" };
+  dbo.collection("dictionary").findOne({}, function(err, result) {
+    if (err) throw err;
+    console.log(result.name);
+    db.close();
+  });
+});
+*/
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+	next();
+});
+
+//app.use(express.static("./public"));
+
+app.use(cors());
+
+app.get("/dictionary-api", function(req, res) {
+	//res.json(skierTerms);
+	//console.log(req.body);
+	const client = new MongoClient(uri, { useNewUrlParser: true });
+	client.connect(err => {
+	const collection = client.db("test").collection("dictionary").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    //console.log(result);
+	autoSearch.push(result);
+	res.json(result);
+   client.close();
+  });
+}); 
+	//autoSearch.push(req.body);
+	//res.json(autoSearch);
+});
+
+app.post("/dictionary-api", function(req, res) {
+    //skierTerms.push(req.body);
+	//res.json(skierTerms);
+	var words=req.body;
+	//printTerms.push(req.body);
+	console.log(req.body);
+	//res.json(req.body);
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+	client.connect(err => {
+	const collection = client.db("test").collection("dictionary").insertOne(req.body, function(err, resp) {
+    if (err) throw err;
+    console.log("1 vocabulary inserted");
+	//autoSearch.push(req.body);
+	//res.json(printTerms);
+   client.close();
+  });
+});
+	client.connect(err => {
+	const collection = client.db("test").collection("dictionary").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+	printTerms.push(result);
+	res.json(result);
+	console.log(req.body);
+   client.close();
+});
+});
+});
+
+app.delete("/dictionary-api/:term", function(req, res) {
+    skierTerms = skierTerms.filter(function(definition) {
+        return definition.term.toLowerCase() !== req.params.term.toLowerCase();
+    });
+    res.json(skierTerms);
+});
+
+
+//app.listen(3000);
+
+//console.log("Express app running on port 3000");
+
+//module.exports = app;
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -28,3 +137,4 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+module.exports = app;
