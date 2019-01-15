c1cf8cb2d167b77a389a2a3641714edc6c2444b0
Sochan Mao <smao@deakin.edu.au>
2019-01-15 13:53:29 +0000

    Working with Core

--- app.js
+++ app.js
@@ -4,11 +4,12 @@
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
+var bodyParser = require('body-parser');
+
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
+
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
@@ -16,117 +17,226 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
-//var express = require("express");
-//var cors = require("cors");
-//var bodyParser = require("body-parser");
-//var app = express();
-
-
-var printTerms=[{}];
-var autoSearch=[{}];
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://admin:admin123456@cluster0-shard-00-00-cmwhk.mongodb.net:27017,cluster0-shard-00-01-cmwhk.mongodb.net:27017,cluster0-shard-00-02-cmwhk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("dictionary").findOne({}, function(err, res) {
-    if (err) throw err;
-    console.log("Result is "+ res.term);
-   client.close();
-  });
- // perform actions on the collection object
-  //client.close();
-  //console.log('Connected to MongoDB');
-});
-/*
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://admin:admin123456@cluster0-shard-00-00-cmwhk.mongodb.net:27017,cluster0-shard-00-01-cmwhk.mongodb.net:27017,cluster0-shard-00-02-cmwhk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("annotator");
- // var myobj = { name: "Thinesh", password:"thinesh1234",address: "Highway 37" };
-  dbo.collection("dictionary").findOne({}, function(err, result) {
-    if (err) throw err;
-    console.log(result.name);
-    db.close();
-  });
-});
-*/
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(function(req, res, next) {
-	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
-	next();
-});
-
-//app.use(express.static("./public"));
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
-app.use(cors());
-
-app.get("/dictionary-get-api", function(req, res) {
-	//res.json(skierTerms);
-	//console.log(req.body);
-	const client = new MongoClient(uri, { useNewUrlParser: true });
-	client.connect(err => {
-	const collection = client.db("test").collection("dictionary").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    //console.log(result);
-	printTerms.push(result);
-	res.json(result);
-   client.close();
-  });
-}); 
-	//autoSearch.push(req.body);
-	//res.json(autoSearch);
-});
-
-app.post("/dictionary-api", function(req, res) {
-    //skierTerms.push(req.body);
-	//res.json(skierTerms);
-	var words=req.body;
-	//printTerms.push(req.body);
-	console.log(req.body);
-	//res.json(req.body);
-    const client = new MongoClient(uri, { useNewUrlParser: true });
-	client.connect(err => {
-	const collection = client.db("test").collection("dictionary").insertOne(req.body, function(err, resp) {
-    if (err) throw err;
-    console.log("1 vocabulary inserted");
-	//autoSearch.push(req.body);
-	//res.json(printTerms);
-	res.redirect('/dictionary-get-api');
-   client.close();
-  });
-});
-	
-});
-
-app.delete("/dictionary-api/:term", function(req, res) {
-    skierTerms = skierTerms.filter(function(definition) {
-        return definition.term.toLowerCase() !== req.params.term.toLowerCase();
-    });
-    res.json(skierTerms);
-});
-
-
-//app.listen(3000);
-
-//console.log("Express app running on port 3000");
-
-//module.exports = app;
-
+app.use(bodyParser.json());
 
 
 // get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+//var appEnv = cfenv.getAppEnv();
+
+
+var session = require('express-session');
+var CloudantStore = require('connect-cloudant-store')(session);
+
+
+var cloudantStore = new CloudantStore({
+  url: 'https://80e12b37-1fae-4c6e-90ab-bb55c8ead656-bluemix:2c1544fb5218812dc2bc5c78f3be2daf6ad8ac31b9ba54d3204496768c782613@80e12b37-1fae-4c6e-90ab-bb55c8ead656-bluemix.cloudant.com',
+  databaseName: 'sessions',
+  prefix: ''
+});
+
+cloudantStore.on('connect', function() {
+  console.log("Cloudant Session store is ready for use");
+});
+
+cloudantStore.on('disconnect', function() {
+  console.log("An error occurred connecting to Cloudant Session Storage");
+});
+
+app.use(session({
+  store: cloudantStore,
+  secret: 'qwerty12',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+            maxAge:365*24*60*60*1000,
+            expires: false},
+}));
+var sess;
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+
+
+
+// load local VCAP configuration  and service credentials
+var vcapLocal;
+try {
+  vcapLocal = require('./vcap-local.json');
+  //console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {};
+
+const appEnv = cfenv.getAppEnv(appEnvOpts);
+
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+
+app.get('/api/core/test', function(req, res){
+
+	var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+	var str = '';
+	for (var vcapService in vcapServices) {
+        if (vcapService.match(/cloudant/i)) {
+            str = vcapServices[vcapService][0].credentials.url;
+        }
+    }
+
+	res.send("Test get cloudant service: " + str);
+	//res.send("Test:" + getDBCredentialsUrl(process.env.VCAP_SERVICES));
+	//res.end();
+});
+
+
+var db;
+
+var cloudant;
+
+
+var dbCredentials = {
+    dbName: 'annotator-b'
+};
+
+//var methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+//app.use(methodOverride());
+
+
+function getDBCredentialsUrl(jsonData) {
+    var vcapServices = JSON.parse(jsonData);
+
+    // Pattern match to find the first instance of a Cloudant service in
+    for (var vcapService in vcapServices) {
+        if (vcapService.match(/cloudant/i)) {
+            return vcapServices[vcapService][0].credentials.url;
+        }
+    }
+}
+
+function initDBConnection() {
+	/*
+    //When running on Bluemix, this variable will be set to a json object
+    //containing all the service credentials of all the bound services
+    if (process.env.VCAP_SERVICES) {
+        dbCredentials.url = getDBCredentialsUrl(process.env.VCAP_SERVICES);
+    } else { //When running locally, the VCAP_SERVICES will not be set
+
+
+    }
+    */
+
+   dbCredentials.url = "https://ebdcae1e-9365-41ed-a418-8212b402e8f9-bluemix:832b9aaf3cf3f743710d220ad4763aea248960261592ed1052600689b88db4b6@ebdcae1e-9365-41ed-a418-8212b402e8f9-bluemix.cloudantnosqldb.appdomain.cloud";
+
+    cloudant = require('cloudant')(dbCredentials.url);
+
+    db = cloudant.use(dbCredentials.dbName);
+}
+
+initDBConnection();
+
+
+function readAllDocuemnts(callback) {
+    db.list({include_docs: true}, function (err, data) {
+        josn_data = JSON.stringify(data);
+        documents = [];
+        console.log(data.total_rows);
+        for (var i = 0; i < data.total_rows; i++) {
+            documents [i] = data.rows[i];
+        }
+        callback(documents);
+    });
+    return null;
+}
+
+// create a document
+function createDocument(document, callback) {
+    //var nextDocID = parseInt(getNumDocuments()) + 1;
+    //console.log('Doc_2#' + nextDocID);
+    word = document.word;
+    definition = document.definition;
+    dict =  document.dict;
+    db.insert({name : word, value: {word: word, definition: definition, dict: dict}}, function (err, data) {
+        console.log('Error:', err);
+        console.log('Data:', data);
+        callback(data);
+    });
+}
+
+// Read All Documents
+app.get('/api/core/allwords', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    readAllDocuemnts(function(data)
+    {
+        res.send(data);
+        res.end();
+    });
+
+});
+
+app.get('/api/core/addword', function (req, res) {
+
+    var sampleDoc = '{"word": "word", "definition": "definition", "dict": "dict"}';
+    sampleDoc = JSON.parse(sampleDoc);
+    createDocument(sampleDoc, function(data) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+        res.end();
+    });
+
+});
+
+// read/search word
+function readDocument(word, callback) {
+    var query = {selector: {name: word}};
+    db.find(query, function (err, data) {
+        console.log('Error:', err);
+        console.log('Data:', data);
+        callback(data);
+    });
+}
+app.get('/api/core/readword', function (req, res) {
+    var result = readDocument("word", function (data) {
+            console.log(data);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(data["docs"]);
+            res.end();
+        }
+    );
+
+});
+function updateDocument(word, dict, callback) {
+    var query = { selector: { name: word}};
+    db.find(query, function(err, data) {
+        console.log('Error:', err);
+        console.log('Data:', data);
+        callback(data);
+    });
+}
+app.get('/api/core/updateword', function (req, res) {
+    var result = updateDocument("word" ,"dict", function(data)
+        {
+            console.log(data);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(data["docs"]);
+            res.end();
+        }
+    );
+
+});
+
 module.exports = app;
