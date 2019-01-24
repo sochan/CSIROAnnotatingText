8531c76567ff48d990ef4cb5e94b4b5172204e31
Sochan Mao <smao@deakin.edu.au>
2019-01-24 05:01:16 +0000

    Update

--- app.js
+++ app.js
@@ -23,79 +23,19 @@ app.use(bodyParser.json());
 
 
 // get the app environment from Cloud Foundry
-//var appEnv = cfenv.getAppEnv();
+var appEnv = cfenv.getAppEnv();
 
 
-var session = require('express-session');
-var CloudantStore = require('connect-cloudant-store')(session);
-
-
-var cloudantStore = new CloudantStore({
-  url: 'https://80e12b37-1fae-4c6e-90ab-bb55c8ead656-bluemix:2c1544fb5218812dc2bc5c78f3be2daf6ad8ac31b9ba54d3204496768c782613@80e12b37-1fae-4c6e-90ab-bb55c8ead656-bluemix.cloudant.com',
-  databaseName: 'sessions',
-  prefix: ''
-});
-
-cloudantStore.on('connect', function() {
-  console.log("Cloudant Session store is ready for use");
-});
-
-cloudantStore.on('disconnect', function() {
-  console.log("An error occurred connecting to Cloudant Session Storage");
-});
-
-app.use(session({
-  store: cloudantStore,
-  secret: 'qwerty12',
-  resave: true,
-  saveUninitialized: false,
-  cookie: {
-            maxAge:365*24*60*60*1000,
-            expires: false},
-}));
-var sess;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-
-
-
-
-// load local VCAP configuration  and service credentials
-var vcapLocal;
-try {
-  vcapLocal = require('./vcap-local.json');
-  //console.log("Loaded local VCAP", vcapLocal);
-} catch (e) { }
-
-const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {};
-
-const appEnv = cfenv.getAppEnv(appEnvOpts);
-
-
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
-});
-
-
-app.get('/api/core/test', function(req, res){
-
-	var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
-	var str = '';
-	for (var vcapService in vcapServices) {
-        if (vcapService.match(/cloudant/i)) {
-            str = vcapServices[vcapService][0].credentials.url;
-        }
-    }
-
-	res.send("Test get cloudant service: " + str);
-	//res.send("Test:" + getDBCredentialsUrl(process.env.VCAP_SERVICES));
-	//res.end();
 });
 
 
@@ -108,13 +48,22 @@ var dbCredentials = {
     dbName: 'annotator-b'
 };
 
-//var methodOverride = require('method-override');
-
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
 app.use(bodyParser.json());
-//app.use(methodOverride());
+
+/*
+ * CORE
+ */
 
 
 function getDBCredentialsUrl(jsonData) {
@@ -142,7 +91,7 @@ function initDBConnection() {
 
    dbCredentials.url = "https://ebdcae1e-9365-41ed-a418-8212b402e8f9-bluemix:832b9aaf3cf3f743710d220ad4763aea248960261592ed1052600689b88db4b6@ebdcae1e-9365-41ed-a418-8212b402e8f9-bluemix.cloudantnosqldb.appdomain.cloud";
 
-    cloudant = require('cloudant')(dbCredentials.url);
+    cloudant = require('cloudant')(dbCredentials.url)
 
     db = cloudant.use(dbCredentials.dbName);
 }
@@ -152,9 +101,9 @@ initDBConnection();
 
 function readAllDocuemnts(callback) {
     db.list({include_docs: true}, function (err, data) {
-        josn_data = JSON.stringify(data);
-        documents = [];
-        console.log(data.total_rows);
+       var josn_data = JSON.stringify(data);
+       var documents = [];
+        //console.log(data.total_rows);
         for (var i = 0; i < data.total_rows; i++) {
             documents [i] = data.rows[i];
         }
@@ -167,12 +116,15 @@ function readAllDocuemnts(callback) {
 function createDocument(document, callback) {
     //var nextDocID = parseInt(getNumDocuments()) + 1;
     //console.log('Doc_2#' + nextDocID);
-    word = document.word;
-    definition = document.definition;
-    dict =  document.dict;
-    db.insert({name : word, value: {word: word, definition: definition, dict: dict}}, function (err, data) {
-        console.log('Error:', err);
-        console.log('Data:', data);
+    var label = document.label;
+    var definition = document.definition;
+    var dict =  document.dictionary;
+    var sel = "0";
+    var link = document.link;
+
+    db.insert({label: label, definition: definition, dict: dict, selected : sel, link : link}, function (err, data) {
+        //console.log('Error:', err);
+        //console.log('Data:', data);
         callback(data);
     });
 }
@@ -190,7 +142,9 @@ app.get('/api/core/allwords', function (req, res) {
 
 app.get('/api/core/addword', function (req, res) {
 
-    var sampleDoc = '{"word": "word", "definition": "definition", "dict": "dict"}';
+    var sampleDoc = '{"label": "label", "definition": "definition", "dict": "dict", "link":""}';
+
+    
     sampleDoc = JSON.parse(sampleDoc);
     createDocument(sampleDoc, function(data) {
         res.setHeader('Content-Type', 'application/json');
@@ -200,43 +154,296 @@ app.get('/api/core/addword', function (req, res) {
 
 });
 
-// read/search word
+// read/search word, seleted
 function readDocument(word, callback) {
-    var query = {selector: {name: word}};
+    var searchWord = word.toLowerCase();//correctSearchWord(word);
+    var query = {selector: {label: searchWord}};
+    console.log(searchWord);
     db.find(query, function (err, data) {
-        console.log('Error:', err);
-        console.log('Data:', data);
+        //console.log('Error:', err);
+        //console.log('Data:', data);
         callback(data);
     });
 }
+
 app.get('/api/core/readword', function (req, res) {
-    var result = readDocument("word", function (data) {
-            console.log(data);
+    var result = readDocument(req.query.searchword, function (data) { //req.body.searchword
+            //console.log(data);
             res.setHeader('Content-Type', 'application/json');
             res.send(data["docs"]);
             res.end();
         }
     );
-
 });
-function updateDocument(word, dict, callback) {
-    var query = { selector: { name: word}};
-    db.find(query, function(err, data) {
-        console.log('Error:', err);
-        console.log('Data:', data);
+
+
+app.post('/api/core/connectOnlineDict', function(req, res){
+    var getOnlineDictionaries = getDefFromAdaptors(req.body.searchword);
+    res.send(getOnlineDictionaries);
+    res.end();
+});
+
+app.get('/api/core/searchword', function (req, res) {
+    //var getOnlineDictionaries = getDefFromAdaptors(req.query.searchword);
+    var result = readDocument(req.query.searchword, function (data) { //req.body.searchword
+        //console.log(data);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data["docs"]);
+        res.end();
+    });
+});
+
+
+
+
+//
+function readSelectedDocument(word, callback) {
+    var searchWord = word.toLowerCase();
+    var query = {selector: {label: searchWord, selected:"1"}};
+    db.find(query, function (err, data) {
+        //console.log('Error:', err);
+        //console.log('Data:', data);
         callback(data);
     });
 }
-app.get('/api/core/updateword', function (req, res) {
-    var result = updateDocument("word" ,"dict", function(data)
-        {
-            console.log(data);
+
+function readDocumentById(docId, callback) {
+    var query = {selector: {_id: docId}};
+    db.find(query, function (err, data) {
+        //console.log('Error:', err);
+        //console.log('Data:', data);
+        callback(data);
+    });
+}
+
+
+app.get('/api/core/getdocbyid', function(req,res){
+    var result = readDocumentById(req.query._id, function (data) { //req.body.searchword
+        //console.log(data);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data["docs"][0]);
+        res.end();
+    }
+);
+})
+
+app.get('/api/core/readselectedword', function (req, res) {
+    var result = readSelectedDocument(req.query.searchword, function (data) { //req.body.searchword
+            //console.log(data);
             res.setHeader('Content-Type', 'application/json');
             res.send(data["docs"]);
             res.end();
         }
     );
+});
+
+function updateDocument(document, callback) {
+     db.insert({"_id":document._id, "_rev": document._rev, "label": document.label, "definition": document.definition, "dict": document.dict,"link":document.link, "selected" : document.selected}, function (err, data) {
+        //console.log('Error:', err);
+        //console.log('Data:', data);
+        callback(data);
+    });
+
+}
+
+app.post('/api/core/updateword', function (req, res) {
+    var document = req.body;
+    var result = updateDocument(document, function(data)
+        {
+            res.send("DONE");
+            res.end();
+        }
+    );
+});
+
+
+// check existed document 
+/**
+ * 
+ * @param [{
+ *  label: "",
+ *  definition: "",
+ *  link: "",
+ *  dictionary: ""
+ * }] documents 
+ * @param {*} callback 
+ */
+function isExisted(documents, callback){
+    var document = documents[0];
+    var nbrow = 0;
+    var query = { selector: { label: document.label, link: document.link}};
+    db.find(query, function(err, data){
+        callback(data);
+    });
+}
+
+/**
+ * Check existed if not insert
+ * @param {*} doc 
+ */
+function isExistedResponse(documents){
+    
+    var result = isExisted(documents, function (data) {
+        //console.log(data);
+        if (data["docs"].length == 0) // not exist then insert
+        {
+            documents.forEach(doc => {
+                createDocument(doc, function(data){
+
+                });
+            });
+        }
+    });
+}
+
+// update selected
+
+/*
+ *  End Core
+ */
+
+/*
+ * Data Analysis
+ */
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+/**
+ * XMLHttRequest from W3School
+ * @param {*} url 
+ */
+function grabUrl(url) {
+    var result;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        result = JSON.parse(this.responseText);
+        // console.log(result);
+      }
+    };
+    xhttp.open("GET", url, false);
+    xhttp.send();
+
+    return result;
+  }
+// update adaptors' API here
+var adaptors = [
+    "https://annotatingtext.appspot.com/api/adaptor/dictionary1/?term=",
+    "https://annotatingtext.appspot.com/api/adaptor/dictionary2/?term=",
+    "https://annotatingtext.appspot.com/api/adaptor/dictionary3/?term=",
+    "https://annotatingtext.appspot.com/api/adaptor/dictionary4/?term="
+];
+
+/**
+ * Get definitions from Adaptors
+ * @param {string} searchword 
+ */
+function getDefFromAdaptors(searchword){
+    searchword = searchword.toLowerCase();
+    var resultFromAdaptor =[];
+    //console.log("searched word: " + searchword)
+    for (var i=0; i < adaptors.length; i++)
+    {
+        var resUrl = grabUrl(adaptors[i] + searchword);
+        if (resUrl.length > 0) //
+        {
+            if (resUrl[0].definition != "")
+            {
+                isExistedResponse(resUrl);// insert list into Cached if not yet
+                resultFromAdaptor.push.apply(resultFromAdaptor, resUrl);
+            }
+                
+        }  
+    }
+	return resultFromAdaptor;
+}
+
+/*
+ * End Data Analysis
+ */
+
+/*
+ * Testing
+ */
+
+app.get('/api/core/test1', function(req, res){
+
+	//res.setHeader('Content-Type', 'application/json');
+	
+	
+	var wrd = getDefDictionary1("food");
+	
+	
+	//res.send(wrd);
+	if (wrd !== "")
+		res.send("Definition: " + wrd.definition);
+	else res.send("Not found");
+	//res.send("Test:" + getDBCredentialsUrl(process.env.VCAP_SERVICES));
+	res.end();
+});
+/**
+ * correct searched word input for example 'fooD' to 'Food'
+ * @param {string} searchWord 
+ */
+
+function correctSearchWord(searchWord){
+    var str = searchWord.toLowerCase();
+    var upper = str.replace(/^\w/, function (chr) {
+        return chr.toUpperCase();
+      });
+    return upper;
+}
+
+app.get('/api/core/test2', function(req, res){
+
+	//res.setHeader('Content-Type', 'application/json');
+	
+	
+	var wrd = getDefDictionary1("food11");
+	
+	
+	//res.send(wrd);
+	if (typeof wrd !== 'undefined')
+		res.send("Definition: " + wrd.definition);
+	else res.send("Not found");
+	//res.send("Test:" + getDBCredentialsUrl(process.env.VCAP_SERVICES));
+	res.end();
+});
+
+
+app.get('/api/core/test3', function(req, res){
+    var str = "fooD";
+    str = str.toLowerCase();
+    var upper = str.replace(/^\w/, function (chr) {
+        return chr.toUpperCase();
+      });
+
+    res.send(upper);
+    res.end();
+})
+
+
+app.get('/api/core/test4', function(req, res){
+    
+    var results = getDefFromAdaptors(req.query.searchword);
+    res.json(results);
+
+    res.end();
+    
 
 });
+
+app.get('/api/core/test5', function(req, res){
+    var doc = {
+        label : 'oxygen',
+        link: 'http://environment.data.gov.au/def/object/oxygen',
+        dict: ''
+    }
+});
+
+
+
+/*
+ * End Testing
+ */
 
 module.exports = app;
